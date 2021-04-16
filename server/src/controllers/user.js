6352a5fa10f9df441bbab60fcfe9dfc1e0df62cb
Gene Koh <73525366+Genekoh@users.noreply.github.com>
2021-04-16 16:53:40 +0700

    updated channel model and added get all message from all channel endpoint

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,8 +1,11 @@
+const ChannelMember = require("../models/channelMember.js");
+const Message = require("../models/message.js");
 const User = require("../models/user.js");
+const throwError = require("../util/throwError.js");
 
 exports.getUserInfo = async (req, res) => {
     try {
-        const username = req.params.username;
+        const { username } = req.params;
 
         const user = await User.findOne({ where: { userName: username } });
     } catch (error) {}
@@ -10,7 +13,47 @@ exports.getUserInfo = async (req, res) => {
 
 exports.getMessages = async (req, res) => {
     try {
-    } catch (error) {}
+        const error = new Error();
+        const { username } = req.user;
+
+        const user = await User.findOne({ where: { userName: username } });
+        if (!user) {
+            throwError(error, 400);
+        }
+
+        const userChannels = await ChannelMember.findAll({
+            where: { UserId: user.id },
+        });
+
+        let messages = [];
+        await Promise.all(
+            userChannels.map(async chan => {
+                const m = await Message.findAll({
+                    where: { ChannelId: chan.ChannelId },
+                });
+
+                // messages = [...messages, ...m];
+                messages = messages.concat(m);
+            }),
+        );
+
+        const formattedOutput = {};
+        userChannels.forEach(chan => {
+            const msg = messages.filter(m => {
+                return m.ChannelId === chan.ChannelId;
+            });
+            formattedOutput[chan.ChannelId] = msg;
+        });
+
+        return res.status(200).json({ ok: true, messages: formattedOutput });
+    } catch (error) {
+        console.log(error);
+        if (!error.status) {
+            return res.status(500).json({ ok: false, messages: {} });
+        }
+
+        return res.status(error.status).json({ ok: false, messages: {} });
+    }
 };
 
 /*
