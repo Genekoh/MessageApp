bb2d766dab73dedcc56710f966c5adbb9a02d6e3
Gene Koh <73525366+Genekoh@users.noreply.github.com>
2021-04-21 18:41:29 +0700

    change to database configurations

--- server/src/database.js
+++ server/src/database.js
@@ -3,6 +3,18 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 console.log(process.env.DATABASE_URL);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: true,
+        },
+    },
+);
 
 module.exports = sequelize;
